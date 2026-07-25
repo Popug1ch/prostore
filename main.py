c74f02faeb6f4f778dfa89cb949a94ded29cc547
МАКС <popugai4iks.maximka@mail.ru>
2026-07-25 20:55:02 +0300

    first routers and create .gitignore

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("START PROSTORE")
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World! It's prostore"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
